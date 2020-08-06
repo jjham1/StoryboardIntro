d506f149876af64a4b1ab06861972561830bde09
jjham1 <63023490+jjham1@users.noreply.github.com>
2020-08-06 11:16:09 -0400

    Add files via upload

--- StoryboardIntro/MiniProject1/MiniProject1/ViewController.swift
+++ StoryboardIntro/MiniProject1/MiniProject1/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
-}
+    @IBOutlet weak var nose: UILabel!
+    @IBOutlet weak var color: UILabel!
+    @IBOutlet weak var tomato: UILabel!
+    
+    @IBAction func firstClick(_ sender: Any) {
+        nose.text = "I can touch my nose w my tongue"
+    }
+    
+    @IBAction func secondClick(_ sender: Any) {
+        color.text = "My favorite color varies by week"
+    }
+    
+    @IBAction func thirdClick(_ sender: Any) {
+        tomato.text = "I hate tomatoes"
+    }
+    
+        
+    }
+    
 
