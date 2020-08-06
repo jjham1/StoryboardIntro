97c737d714b9277d5d7c5bc0274ae11e19917227
jjham1 <63023490+jjham1@users.noreply.github.com>
2020-08-05 21:16:52 -0400

    Miniproject1

---
+++ StoryboardIntro/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Julie Ham on 8/5/20.
+//  Copyright © 2020 Julie Ham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
